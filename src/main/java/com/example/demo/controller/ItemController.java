3719b1068d4bc923ed9885f20f7285a9d56aae7c
tsuruoka <kazuto1tsuruoka1@gmail.com>
2025-06-10 20:01:30 +0900

    items.html,itemController,main.jsの編集

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -2,7 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+
+// 非同期じゃないコントローラー
 @Controller
 public class ItemController {
     
@@ -12,7 +17,11 @@ public String index() {
 }
 
 
-    
+}
 
+// 非同期用のコントローラー
+@RestController
+@RequestMapping("/api/items")
+public class apiController {
 
 }
